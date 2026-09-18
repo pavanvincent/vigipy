536d5f35dd7a81f048b741d70564033146f11283
pavanvincent <93097908+pavanvincent@users.noreply.github.com>
2026-09-18 13:06:25 +0200

    Sort DataFrame by 'LB(IC 95%)' in descending order
    Change sorting order of values in DataFrame to descending.

--- src/vigipy/ROR/ROR.py
+++ src/vigipy/ROR/ROR.py
@@ -135,7 +135,7 @@ def ror(
             # "fdr": FDR,
         },
         index=np.arange(len(n11)),
-    ).sort_values(by=["LB(IC 95%)"])
+    ).sort_values(by=["LB(IC 95%)"], ascending = False)
 
     if ranking_statistic == "CI":
         RC.all_signals = RC.all_signals.rename(columns={"p_value": "lower_bound_CI(95%)"}).sort_values(
