6e93421614a29dd03726ea26891235a27b4c3723
pavanvincent <93097908+pavanvincent@users.noreply.github.com>
2026-09-18 12:39:17 +0200

    Update sorting column name in ROR.py

--- src/vigipy/ROR/ROR.py
+++ src/vigipy/ROR/ROR.py
@@ -135,7 +135,7 @@ def ror(
             # "fdr": FDR,
         },
         index=np.arange(len(n11)),
-    ).sort_values(by=["LB"])
+    ).sort_values(by=["LB(IC 95%)"])
 
     if ranking_statistic == "CI":
         RC.all_signals = RC.all_signals.rename(columns={"p_value": "lower_bound_CI(95%)"}).sort_values(
