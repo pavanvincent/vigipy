d7ce66a47896b6320c225305a032cc44da393640
pavanvincent <93097908+pavanvincent@users.noreply.github.com>
2026-09-22 02:59:36 +0200

    Refactor num_signals calculation in BCPNN.py

--- src/vigipy/BCPNN/BCPNN.py
+++ src/vigipy/BCPNN/BCPNN.py
@@ -147,7 +147,7 @@ def bcpnn(
     ).sort_values(by=["lower_bound"], ascending=False)
     RC.signals = RC.all_signals.loc[RC.all_signals["lower_bound"] > 1]
     
-    num_signals = (lower_bound > 0).sum()
+    num_signals = len(RC.signals)
     
     if num_signals > 0:
         num_signals -= 1
