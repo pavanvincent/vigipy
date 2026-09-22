1b4e9a75cdc4018793b89507028523c170e8a197
pavanvincent <93097908+pavanvincent@users.noreply.github.com>
2026-09-22 15:38:01 +0200

    Refactor ranking statistic and minimization method
    Updated ranking statistic and minimization method. Adjusted calculations for lower and upper bounds based on new ranking methods and removed unused calculations.

--- src/vigipy/GPS/GPS.py
+++ src/vigipy/GPS/GPS.py
@@ -35,7 +35,7 @@ def gps(
     min_events=1,
     decision_metric="rank",
     decision_thres=0.05,
-    ranking_statistic="quantile",
+    ranking_statistic="log2",
     truncate=False,
     truncate_thres=1,
     prior_init={
@@ -48,7 +48,7 @@ def gps(
     prior_param=None,
     expected_method="mantel-haentzel",
     method_alpha=1,
-    minimization_method="CG",
+    minimization_method="SLSQP",
     minimization_bounds=((EPS, 20), (EPS, 10), (EPS, 20), (EPS, 10), (0, 1)),
     minimization_options=None,
 ):
@@ -210,8 +210,8 @@ def gps(
     dgterm2 = dg2 - np.log(priors[3] + expected)
     EBlog2 = (np.log(2) ** -1) * (Qn * dgterm1 + (1 - Qn) * dgterm2)
 
-    # Calculation of the Lower Bound at 5% (FDA standards)
-    LB05 = quantiles(
+    # Calculation of the Lower Bound.
+    LB = quantiles(
         0.05,
         Qn,
         priors[0] + n11,
@@ -220,48 +220,43 @@ def gps(
         priors[3] + expected,
     )
 
-     # Calculation of the Lower Bound at 2,5% (WHO standards)
-    LB025 = quantiles(
-        0.025,
-        Qn,
-        priors[0] + n11,
-        priors[1] + expected,
-        priors[2] + n11,
-        priors[3] + expected,
-    )
-
-    # Calculation of the Upper Bound at 95% (FDA standards)
-    UB95 = quantiles(
-        0.95,
-        Qn,
-        priors[0] + n11,
-        priors[1] + expected,
-        priors[2] + n11,
-        priors[3] + expected,
-    )
-
-     # Calculation of the Upper Bound at 97,5% (WHO standards)
-    UB975 = quantiles(
-        0.975,
-        Qn,
-        priors[0] + n11,
-        priors[1] + expected,
-        priors[2] + n11,
-        priors[3] + expected,
-    )
+    # Assignment based on the method
+    if ranking_statistic == "p_value":
+        RankStat = posterior_probability
+    elif ranking_statistic == "quantile":
+        RankStat = LB
+    elif ranking_statistic == "log2":
+        RankStat = np.array([x.evalf() for x in EBlog2])
 
     post_cumsum = np.cumsum(posterior_probability)
     post_1_cumsum = np.cumsum(1 - posterior_probability)
     post_1_sum = sum(1 - posterior_probability)
     post_range = np.arange(1, len(posterior_probability) + 1)
 
-    FDR = post_cumsum / post_range
-    FNR = np.array(list(reversed(post_1_cumsum))) / ((num_cell - post_range) + 1e-7)
-    Se = np.cumsum((1 - posterior_probability)) / post_1_sum
-    Sp = np.array(list(reversed(post_cumsum))) / (num_cell - post_1_sum)
+    if ranking_statistic == "p_value":
+        FDR = post_cumsum / np.array(post_range)
+        FNR = np.array(post_1_cumsum) / ((num_cell - post_range) + 1e-7)
+        Se = np.cumsum((1 - posterior_probability)) / post_1_sum
+        Sp = np.array(post_cumsum) / (num_cell - post_1_sum)
+    else:
+        FDR = post_cumsum / post_range
+        FNR = np.array(list(reversed(post_1_cumsum))) / ((num_cell - post_range) + 1e-7)
+        Se = np.cumsum((1 - posterior_probability)) / post_1_sum
+        Sp = np.array(list(reversed(post_cumsum))) / (num_cell - post_1_sum)
 
-    RankStat = LB05
-    ranking_statistic == "quantile"
+    # Number of signals according to the decision rule (pp/FDR/Nb of Signals)
+    if decision_metric == "fdr":
+        num_signals = np.sum(FDR <= decision_thres)
+    elif decision_metric == "signals":
+        num_signals = min((RankStat <= decision_thres).sum(), num_cell)
+    elif decision_metric == "rank":
+        if ranking_statistic == "p_value":
+            num_signals = np.sum(RankStat <= decision_thres)
+        elif ranking_statistic == "quantile":
+            num_signals = np.sum(RankStat >= decision_thres)
+        elif ranking_statistic == "log2":
+            num_signals = np.sum(RankStat >= decision_thres)
+
     name = DATA["product_name"]
     ae = DATA["ae_name"]
     count = n11
@@ -273,27 +268,63 @@ def gps(
     RES.param["convergence"] = code_convergence
 
     # SIGNALS RESULTS and presentation
-    RES.all_signals = pd.DataFrame(
-        {
-            "Product": name,
-            "Adverse Event": ae,
-            "quantile": RankStat,
-            "EBGM" : np.float64(2**EBlog2),
-            "EB025": LB025,
-            "EB05" : LB05,
-            "EB95" : UB95,
-            "EB975": UB975,
-            "Count": count,
-            "product margin": n1j,
-            "event margin": ni1,
-            "fdr": FDR,
-            "FNR": FNR,
-            "Se": Se,
-            "Sp": Sp,
+    if ranking_statistic == "p_value":
+        RES.all_signals = pd.DataFrame(
+            {
+                "Product": name,
+                "Adverse Event": ae,
+                "Count": count,
+                "Expected Count": expected,
+                "p_value": RankStat,
+                "count/expected": (count / expected),
+                "product margin": n1j,
+                "event margin": ni1,
+                "fdr": FDR,
+                "FNR": FNR,
+                "Se": Se,
+                "Sp": Sp,
+            }
+        ).sort_values(by=[ranking_statistic])
+
+    elif ranking_statistic == "quantile":
+        RES.all_signals = pd.DataFrame(
+            {
+                "Product": name,
+                "Adverse Event": ae,
+                "Count": count,
+                "Expected Count": expected,
+                "quantile": RankStat,
+                "count/expected": (count / expected),
+                "product margin": n1j,
+                "event margin": ni1,
+                "fdr": FDR,
+                "FNR": FNR,
+                "Se": Se,
+                "Sp": Sp,
+                "posterior_probability": posterior_probability,
             }
         )
         RES.all_signals = RES.all_signals.sort_values(by=[ranking_statistic], ascending=False)
-
+    else:
+        RES.all_signals = pd.DataFrame(
+            {
+                "Product": name,
+                "Adverse Event": ae,
+                "Count": count,
+                "Expected Count": expected,
+                "log2": RankStat,
+                "count/expected": (count / expected),
+                "product margin": n1j,
+                "event margin": ni1,
+                "fdr": FDR,
+                "FNR": FNR,
+                "Se": Se,
+                "Sp": Sp,
+                "LowerBound": LB,
+                "p_value": posterior_probability,
+            }
+        )
+        RES.all_signals = RES.all_signals.sort_values(by=[ranking_statistic], ascending=False)
 
     # List of Signals generated according to the method
     RES.all_signals.index = np.arange(0, len(RES.all_signals.index))
